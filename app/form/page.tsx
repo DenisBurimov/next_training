5b1e578d32a0c157b2a6149884e9aea8edaea051
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2024-03-27 19:01:24 +0100

    work: add yup

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,22 +1,23 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { userSchema } from "../validations/UserValidations";
 
 export default function LoginForm() {
-    return (
-        <div>
-            <h1 className="text-center">Login Page</h1>
-            <div className="flex justify-center">
-                <form className='p-5 border border-black'>
-                    <div className="flex flex-col">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name="email" className='border border-black' />
+  return (
+    <div>
+      <h1 className="text-center">Login Page</h1>
+      <div className="flex justify-center">
+        <form className="p-5 border border-black">
+          <div className="flex flex-col">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" className="border border-black" />
 
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name="password" className='border border-black' />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" className="border border-black" />
 
-                        <button type="submit">Submit</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
