48961e23b7f37c9c9bf885aa13809ae233df2f10
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2024-04-02 07:34:21 +0200

    fix image

--- app/ui/cutdown_main/cutdown-main.tsx
+++ app/ui/cutdown_main/cutdown-main.tsx
@@ -4,56 +4,59 @@ import { useState } from "react";
 import Image from "next/image";
 import { games } from "@/app/lib/data";
 import { formatDate } from "@/app/lib/utils";
+import clsx from "clsx";
 
 export default function CutdownMain() {
   const [currentTab, setCurrentTab] = useState(1);
 
   const toggleTab = (index: number) => {
-    console.log("index", index);
-
     setCurrentTab(index);
   };
   return (
-    <>
-      <div className="relative flex flex-col col-span-9">
+    <div className="relative flex flex-col col-span-9">
+      <Image
+        className="absolute z-0 w-full h-full"
+        src={"/cs.jpeg"}
+        width={3000}
+        height={1500}
+        alt="game image"
+      ></Image>
+      {games.map((game, i) => (
         <Image
-          className="absolute z-0 w-full h-full"
-          src={"/cs.jpeg"}
+          key={game.id}
+          className={clsx("relative", i !== currentTab && "hidden")}
+          src={game.image_path}
           width={3000}
           height={1500}
           alt="game image"
         ></Image>
-        <Image className="relative" src={"/cs.jpeg"} width={3000} height={1500} alt="game image"></Image>
-        <div className="relative flex backdrop-blur-xl border-t border-white border-opacity-5 bg-grey-800/20">
-          {games.map((game, i) => {
-            return (
-              <div
-                key={game.id}
-                className="h-40 p-2 w-full flex flex-col gap-1 cursor-pointer"
-                onClick={() => toggleTab(i)}
-              >
-                {/* <Image
-                  className="absolute w-full z-0"
-                  src={game.image_path}
-                  width={100}
-                  height={100}
-                  alt="game image"
-                ></Image> */}
-                <span className="text-sm">{game.name_tournament}</span>
-                <span className="text-sm text-gray-500">{formatDate(game.date)}</span>
-                <div className="flex gap-4 items-center">
-                  <Image className="" src={game.logo_team_1} width={50} height={50} alt="game image"></Image>
-                  <span className="text-gray-500">{game.name_team_1}</span>
-                </div>
-                <div className="flex gap-4 items-center">
-                  <Image className="" src={game.logo_team_2} width={50} height={50} alt="game image"></Image>
-                  <span className="text-gray-500">{game.name_team_2}</span>
-                </div>
+      ))}
+      <div className="relative flex backdrop-blur-xl border-t border-white border-opacity-5 bg-grey-800/20">
+        {games.map((game, i) => {
+          return (
+            <div
+              key={game.id}
+              className={clsx(
+                "h-40 p-2 w-full flex flex-col gap-1 cursor-pointer",
+                i !== 2 && "border-r border-white border-opacity-5 bg-grey-800/20",
+                i === currentTab && "bg-black"
+              )}
+              onClick={() => toggleTab(i)}
+            >
+              <span className="text-sm">{game.name_tournament}</span>
+              <span className="text-sm text-gray-500">{formatDate(game.date)}</span>
+              <div className="flex gap-4 items-center">
+                <Image className="" src={game.logo_team_1} width={50} height={50} alt="game image"></Image>
+                <span className="text-gray-500">{game.name_team_1}</span>
               </div>
-            );
-          })}
-        </div>
+              <div className="flex gap-4 items-center">
+                <Image className="" src={game.logo_team_2} width={50} height={50} alt="game image"></Image>
+                <span className="text-gray-500">{game.name_team_2}</span>
+              </div>
+            </div>
+          );
+        })}
       </div>
-    </>
+    </div>
   );
 }
